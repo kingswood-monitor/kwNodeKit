53cbab63d6dcb1a1922c360debe22b93f58c63b5
Richard Lyon <richardlyon@fastmail.com>
2020-09-13 12:14:36 +0100

    add timestamp parameter to sendPacket

--- src/base/kwTransport.h
+++ src/base/kwTransport.h
@@ -18,8 +18,8 @@ class kwTransport
 {
 public:
     kwTransport();
-    virtual bool startTransport() = 0;                                        // start the radio
-    virtual bool sendPacket(uint8_t *packetBuffer, uint8_t bytesWritten) = 0; // send a packet
+    virtual bool startTransport() = 0;                                                              // start the radio
+    virtual bool sendPacket(uint16_t uiTimeStamp, uint8_t *packetBuffer, uint8_t bytesWritten) = 0; // send a packet
     void isGateway(bool flag);
     bool isGateway();
 
--- src/transport/kwLoraSM.h
+++ src/transport/kwLoraSM.h
@@ -19,7 +19,7 @@ public:
 
     // kwTransport interface
     bool startTransport();
-    bool sendPacket(uint8_t *packetBuffer, uint8_t bytesWritten);
+    bool sendPacket(uint16_t uiTimeStamp, uint8_t *packetBuffer, uint8_t bytesWritten);
 
     // kwSensor interface
     bool startSensor();
